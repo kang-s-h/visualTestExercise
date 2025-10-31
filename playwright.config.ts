2b4fb375d19ae8815a5b1972b426ee02f4e1ff50
강성훈 <kshevl123@gmail.com>
2025-10-31 23:35:46 +0900

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
-  
+  snapshotPathTemplate: '{testDir}/{testFileDir}/{arg}{ext}',
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
